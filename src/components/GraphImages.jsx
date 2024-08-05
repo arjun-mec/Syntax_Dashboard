e3e191a8dc2d468a34c9835a138cdb60d5c7b535
arjun-mec <arjunajayan2005@gmail.com>
2024-08-05 09:17:36 +0530

    image edited

--- src/components/GraphImages.jsx
+++ src/components/GraphImages.jsx
@@ -5,6 +5,12 @@ import "slick-carousel/slick/slick-theme.css";
 import "../components/GraphImages.css";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import image1 from "../assets/image1.png";
+import image2 from "../assets/image2.png";
+import image3 from "../assets/image3.png";
+import image4 from "../assets/image4.png";
+import image5 from "../assets/image5.png";
+import image6 from "../assets/image6.png";
 
 const GraphImages = () => {
   var settings = {
@@ -23,46 +29,22 @@ const GraphImages = () => {
   return (
     <Slider {...settings} className="image-slider">
       <div>
-        <img
-          src="src\assets\image1.png"
-          alt="Graph 1"
-          className="graph-image1"
-        />
+        <img src={image1} alt="Graph 1" className="graph-image1" />
       </div>
       <div>
-        <img
-          src="src\assets\image2.png"
-          alt="Graph 1"
-          className="graph-image2"
-        />
+        <img src={image2} alt="Graph 2" className="graph-image2" />
       </div>
       <div>
-        <img
-          src="src\assets\image3.png"
-          alt="Graph 1"
-          className="graph-image3"
-        />
+        <img src={image3} alt="Graph 3" className="graph-image3" />
       </div>
       <div>
-        <img
-          src="src\assets\image4.png"
-          alt="Graph 1"
-          className="graph-image4"
-        />
+        <img src={image4} alt="Graph 4" className="graph-image4" />
       </div>
       <div>
-        <img
-          src="src\assets\image5.png"
-          alt="Graph 1"
-          className="graph-image5"
-        />
+        <img src={image5} alt="Graph 5" className="graph-image5" />
       </div>
       <div>
-        <img
-          src="src\assets\image6.png"
-          alt="Graph 1"
-          className="graph-image6"
-        />
+        <img src={image6} alt="Graph 6" className="graph-image6" />
       </div>
     </Slider>
   );
